1c2b200a9274cda2c30265776dab0ee61deb766d
Abednego Emonena <emonenaabednego@gmail.com>
2022-06-29 11:42:52 -0700

    a c code thats prints out a text using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - entry point of the program
+ *
+ * Return: returns 0 as success
+ *
+ * Description: prints out a text using printf function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	returns (0);
+
+}
