4626df628a2e1cc29724081f50368880e5bfd853
Abednego Emonena <emonenaabednego@gmail.com>
2022-06-29 11:36:02 -0700

    a c code that prints out text using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point of the program
+ *
+ * Return: returns 0 (success)
+ *
+ * Description: the program will prints outs a text
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
