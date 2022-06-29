66cc2ee6aa5389bd3623dd490ad0a1f42ec84ace
Abednego Emonena <emonenaabednego@gmail.com>
2022-06-29 12:07:57 -0700

    a c code that prints the size of c types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  main - Entry point of the program
+ *
+ *  Return: returns 0 as success
+ *
+ *  Description: prints out the size of types in byte
+ */
+
+
+int main (void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float %d byte(s)\n", sizeof(float));
+	returns (0);
+
+}
+
