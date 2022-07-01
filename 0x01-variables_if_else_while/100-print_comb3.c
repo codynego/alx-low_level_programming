b15035ccd6eac1001ad7632da0b32d5c2b5960a9
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-01 19:05:09 +0000

    added 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of two digit
+ *
+ * Return: returns 0 as success
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int p;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (p = 49; p < 58; p++)
+		{
+			putchar(n);
+			putchar(p);
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+
+}
