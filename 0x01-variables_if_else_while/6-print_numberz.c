a0d1a792bc7aecbfab79835929954779a784f933
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-01 16:22:53 +0000

    added 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all single digit of base 10
+ *
+ * Return: returns 0 as success
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
