adcccc70a341f3cbd917f766671e190ba7b7c68f
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-01 16:52:59 +0000

    added 7-print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in the reverse form
+ *
+ * Return: returns 0 as success
+ *
+ */
+
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i = 0; i--)
+	{
+		putchar(alphabets[i]);
+
+	}
+	putchar('\n');
+	return (0);
+
+}
