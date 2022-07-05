e19700aa905aa6a8214f99253424f075494c0892
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 09:27:18 -0700

    added 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void print_alphabet - prints all alphabet in lower case
+ *
+ * Return: return void
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+
+	}
+		_putchar('\n');
+
+}
