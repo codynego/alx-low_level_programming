8c04f28b04a2de279fa1ba9969e0b0322d78af31
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 19:20:43 +0100

    added _islower file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** 
+ * _islower - check ir a character is lowercase
+ *
+ * Return: return 1 if lowercase
+ */
+
+int _islower(int c)
+{
+	int i;
+
+	if (i >= 97 && i <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
