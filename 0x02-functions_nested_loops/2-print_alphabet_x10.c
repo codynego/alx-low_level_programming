a11988c6c8b5c2735422ba38e71629b73051eb01
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 10:12:14 -0700

    made a few changes to the filee

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-	char i;
+	int i,j;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (i = 97; i < 122; i++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(i);
 		}
-			_putchar('\n');
+			
+		_putchar('\n');
 	}
 }
