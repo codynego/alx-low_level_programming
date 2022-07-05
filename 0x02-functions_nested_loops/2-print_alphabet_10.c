789d6c5d426480120f1ce28786d9b36bbae16769
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 18:55:32 +0100

    hot fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lower case alphabet x10 
+ *
+ * Return: return 0 as success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+
+		}
+		_putchar('\n');
+	}
+}
