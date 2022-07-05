0fbb7a543be85479a152a26367f700a595cc613b
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 23:22:21 +0100

    added 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * add - print the absolute value of number
+ *
+ * @i: character to be checked
+ *
+ * Return: return the absolute value
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
