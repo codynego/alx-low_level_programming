5f9506a5464e3e158b25b3ca6cd77c8f6c8252bf
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 19:56:37 +0100

    added 4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * _isalpha - check ir a character is lowercase
+ *
+ * @c: character to be checked
+ *
+ * Return: return 1 if lowercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+}
