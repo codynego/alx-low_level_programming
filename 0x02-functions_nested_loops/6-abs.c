f44dec4e655a8459b896268c027f33aec721aff4
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 21:00:26 +0100

    added 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * print_sign - print the absolute value of number
+ *
+ * @i: character to be checked
+ *
+ * Return: return the absolute value
+ *
+ */
+
+int print_sign(int i)
+{
+	if (i < 0)
+	{
+
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+
+	}
+
+}
