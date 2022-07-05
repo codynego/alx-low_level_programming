0f43ca48137c9ceb62058d9920c8ec83f6185a5b
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-05 22:40:44 +0100

    quick fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,12 +17,14 @@ void jack_bauer(void)
 
 	for (a = 48; a <= 50; a++)
 	{
-		for (b = 48; b <= 51; b++)
+		for (b = 48; b <= 57; b++)
 		{
 			for (c = 48; c <= 53; c++)
 			{
 				for (d = 48; d <= 57; d++)
 				{
+					if (a >= 50 && b >= 52)
+						break;
 					_putchar(a);
 					_putchar(b);
 					_putchar(':');
