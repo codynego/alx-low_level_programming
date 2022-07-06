6133a31b0add491c804215d19ff76e434d3484dd
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-06 12:10:27 +0100

    added the 0-main.c file that uses positive_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print if a number is positive or not using the positive_or_negative function
+ *
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
