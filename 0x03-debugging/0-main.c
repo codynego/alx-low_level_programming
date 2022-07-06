6e68abd2ad0e24e7f461c4efe77e11bf5649a87e
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-06 14:42:40 +0100

    added few files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - print if a number is positive or not using the positive_or_negative function
- *
- * Return: return 0 as success
- */
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 
 int main(void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
-
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,5 +1,1 @@
-README
-
-this directory is to learn and understand the importance of debugging 
-
-it helps to teach how to debug a c program
+Debugging
