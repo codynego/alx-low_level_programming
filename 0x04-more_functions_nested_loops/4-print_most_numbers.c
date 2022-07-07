d811203383f9cfae9e78580010d1027e94b932c6
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-07 08:14:45 +0100

    added 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0-9
+ *
+ *
+ * Return: return 0 as success
+ */
+
+void print_most_numbers(void);
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a == 50 || a == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a);
+		}
+	}
+
+		_putchar('\n');
+
+}
+
+
