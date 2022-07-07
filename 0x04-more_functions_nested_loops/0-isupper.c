0a38ed1e055a5acf98100a9eddcc88fff4de6811
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-07 07:21:04 +0100

    added 0_isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - check is a number is upper case or not
+ * @c: charater to be checked
+ *
+ * Return: return 0 as success
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 || c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
