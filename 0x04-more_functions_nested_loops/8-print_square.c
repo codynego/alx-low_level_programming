1cac8e073634510e765ea21402fd6538d332f1d9
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-07 12:37:49 +0100

    hotfix

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_square - print '#' n times
+ *
+ * @size: charater to be checked
+ *
+ * Return: return 0 as success
+ */
+
+void print_square(int size);
+{
+	int i;
+	int b = 0;
+
+	do
+	{
+
+		for (i = 0; i <= size; i++)
+		{
+			if (size <= 0)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar('#');
+
+			}
+	
+				_putchar('\n');
+		}
+
+			b++;
+			putchar('\n');
+	}
+	while (b < size);
+}
+
+
+
