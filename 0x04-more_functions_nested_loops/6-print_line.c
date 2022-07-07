e3ca2786c7bb9b8a008a9293f7b9aa6a07cbcd02
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-07 09:27:29 +0100

    added 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - print '_' n times
+ *
+ * @n: charater to be checked
+ *
+ * Return: return 0 as success
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+			_putchar('\n');
+
+}
+
+
