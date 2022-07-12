955cdb7b629b00d6efc9d30965f550c63663bb42
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-12 13:53:51 +0000

    added 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string in reverse
+ *
+ * @s: input integer
+ *
+ * Return: Always void
+ *
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+	int i;
+
+	while (*(s + count) != '\0')
+	{
+		_putchar(*(s + count));
+		count++;
+	}
+		
+	for (i = count; i == 0; i--)
+	{
+		_putchar(*(s + count);
+	}
+
+	_putchar('\n');
+}
