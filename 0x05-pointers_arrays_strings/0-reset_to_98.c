2a3b1e4bff89425fa9a1e8d2f05cc78dbe66641f
Abednego Emonena <emonenaabednego@gmail.com>
2022-07-12 10:39:37 +0000

    added 0-reset_to_98.c file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98: a function that takes an integer and update to 98
+ * reset_to_98: a function that takes an integer and update to 98.
  *
- * @n: input integer
+ * @n: input integer.
  *
- * Return: Always void
+ * Return: Always void.
+ *
+ * Description: change the integer to 98.
  */
 
 void reset_to_98(int *n)
