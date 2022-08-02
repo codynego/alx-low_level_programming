884ea3452f69ac7a7db1ca2b0753b629c3809184
Abednego Emonena <emonenaabednego@gmail.com>
2022-08-02 14:38:14 +0000

    added 2-print_dog file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include "stdio.h"
+
+/**
+ * print_dog - a function that prints a struct dog
+ *
+ * @d: a struct dog
+ * Return: no return
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age == NULL)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %d\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
+
