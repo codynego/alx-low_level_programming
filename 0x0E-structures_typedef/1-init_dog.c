cb1c0161ca805ac37f0de9b4fc77cf96028f0cfb
Abednego Emonena <emonenaabednego@gmail.com>
2022-08-02 13:22:38 +0000

    added 1-init_dog.c file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize  a variablr of type struct dog
+ *
+ * @d: struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: returns (0)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	return (0);
+}
