22771ee69a72ac4a3dafd7ec59f7cb9c198d7664
Abednego Emonena <emonenaabednego@gmail.com>
2022-10-03 23:42:43 +0000

    added a few files

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,63 @@
+#include "operations.h"
+
+/**
+ * add - add two numbers
+ * @a: the 1st number
+ * @b: the 2nd number
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+  return (a + b);
+}
+
+
+/**
+ * sub - subtract two numbers
+ * @a: the 1st number
+ * @b: the 2nd number
+ *
+ * Return: the difference of a and b
+ */
+int sub(int a, int b)
+{
+  return (a - b);
+}
+
+
+/**
+ * mul - multiply two numbers
+ * @a: the 1st number
+ * @b: the 2nd number
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: the 1st number
+ * @b: the 2nd number
+ *
+ * Return: the division of a and b
+ */
+int div(int a, int b)
+{
+  return (a / b);
+} 
+
+/**
+ * mod - remainder of division of two numbers
+ * @a: the 1st number
+ * @b: the 2nd number
+ *
+ * Return: the remiander of division of a and b
+ */
+int mod(int a, int b)
+{
+  return (a % b);
+} 
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef _OPERATIONS_H_
+#define _OPERATIONS_H_
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* OPERATIONS_H */
